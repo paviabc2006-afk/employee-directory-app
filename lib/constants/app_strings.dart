b7ec9f4fa647f7aa2b31fe8d63ecad425a97b67e
paviabc2006-afk <paviabc2006@gmail.com>
2026-06-09 16:07:56 +0530

    commited code changes for user profile widgets

---
+++ lib/constants/app_strings.dart
@@ -0,0 +1,12 @@
+﻿class AppStrings {
+  static const String appName = 'Flutter app';
+  static const String welcomeMessage = 'Welcome Back!';
+  static const String userName = 'Aravind';
+  static const String homeTab = 'Home';
+  static const String employeesTab = 'Employees';
+  static const String profileTab = 'Profile';
+  static const String employeeDetails = 'Employee Details';
+  static const String settings = 'Settings';
+  static const String editProfile = 'Edit Profile';
+  static const String openSettings = 'Settings';
+}
